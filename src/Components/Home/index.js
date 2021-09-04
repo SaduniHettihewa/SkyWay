5b3d3893b87fc10ba5a958c62b3ca7def9e36f50
SaduniHettihewa <hhsnimasha98@gmail.com>
2021-09-04 23:10:09 +0530

    complete home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,7 +4,10 @@ import './Home.css'
 function Home() {
     return (
         <div className="home">
-            <img src={require("../../Assests/home.jpg").default} className="Walpaper"></img>
+            {/* <img src={require("../../Assests/home.jpg").default} className="Walpaper"></img> */}
+            <div className="logo">
+            <img src={require("../../Assests/logo.png").default} className="logo"></img>
+            </div>
         </div>
     )
 }
