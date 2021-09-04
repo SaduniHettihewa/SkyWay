c932422e12d4d864c499528eb6543835d8e99c51
SaduniHettihewa <hhsnimasha98@gmail.com>
2021-09-04 22:53:23 +0530

    add walpaper

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './Home'
+import Home from './Components/Home/index'
 
 function App() {
   return (
---
+++ src/Components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <img src={require("../../Assests/home.jpg").default} className="Walpaper"></img>
+        </div>
+    )
+}
+
+export default Home
