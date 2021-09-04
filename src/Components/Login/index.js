ba25451501b8e05a38b7e7d1fc0357ef789ed0b0
SaduniHettihewa <hhsnimasha98@gmail.com>
2021-09-05 02:15:53 +0530

    login page

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -2,14 +2,47 @@ import React from 'react'
 import './Login.css'
 
 function Login() {
-    return (
-        <div className="Login">
+  return (
+    <div className="Login">
       <div>
         <img src={require("../../Assests/logo.png").default} className="LoginLogo"></img>
-        </div>
-            
-        </div>
-    )
+      </div>
+
+      < div  >
+
+        <form className="loginView" >
+          <div className="LoginLable">
+            Login
+          </div>
+
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            placeholder="Enter your email"
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <div>
+            <button type="submit" className="Loginbutton" >
+              Login
+            </button>
+          </div>
+          <div>
+            <lable className="account"> Don't have an account ?  SignUp </lable>
+          </div>
+        </form>
+      </div>
+    </div>
+
+  )
 }
 
 export default Login
