2077f8805f6643ae3bdd9638834e614283ae0a24
SaduniHettihewa <hhsnimasha98@gmail.com>
2021-09-04 23:23:52 +0530

    modify home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,10 +4,15 @@ import './Home.css'
 function Home() {
     return (
         <div className="home">
-            {/* <img src={require("../../Assests/home.jpg").default} className="Walpaper"></img> */}
             <div className="logo">
             <img src={require("../../Assests/logo.png").default} className="logo"></img>
             </div>
+            <div className="buttonPlace">
+                <button className="button">
+                    Let's Go
+                </button>
+            </div>
+
         </div>
     )
 }
