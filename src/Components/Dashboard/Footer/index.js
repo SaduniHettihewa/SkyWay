263abb7442c497be73f762e6adac6c1bfeac5579
SaduniHettihewa <hhsnimasha98@gmail.com>
2021-09-13 12:29:35 +0530

    complete own note

--- src/Components/Dashboard/Footer/index.js
+++ src/Components/Dashboard/Footer/index.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 import './Footer.css'
 import TodoList from './TodoList';
 import {v4 as uuidv4} from 'uuid'
 
 //tido list adding 
 function Footer () {
+    const initialState = JSON.parse(localStorage.getItem("todos"))|| [];
 const[input ,setInput]=useState("");
-const[todos ,setTodos]=useState([]);
+const[todos ,setTodos]=useState(initialState);
 
+useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+}, [todos]);
 
   const onInputChange =(event) =>{
       setInput(event.target.value);
   }
   const addItem=(event)=>{
        event.preventDefault();
+       
        setTodos([...todos,{ id:uuidv4(),title:input , completed:false}]);
        setInput("");
   }
